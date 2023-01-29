2c39a482ac719d819cf8a421f4369a274d9c6a2e
Matias Seitour <matias.seitour01@gmail.com>
2023-01-29 19:07:39 -0300

    feat(client/main) add context provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
+import {TasksContextProvider} from './Context/TasksContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <TasksContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </TasksContextProvider>
   </React.StrictMode>,
+
 )
