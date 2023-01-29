8d5261b4f5ccdcc17624bacda6e2925da4532e3d
Matias Seitour <matias.seitour01@gmail.com>
2023-01-29 19:10:04 -0300

    feat(client/app) remove Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import './App.css'
+import './styles.css'
 import { Route, Routes } from 'react-router-dom'
-import {Tasks} from "./components/Tasks"
+import { Tasks } from "./components/Tasks"
 import { NewTask } from './components/NewTask'
 import { NotFound } from './components/NotFound'
 import { Navbar } from './components/Navbar'
@@ -10,11 +9,10 @@ function App() {
 
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path='/' element={<Tasks />} />
-        <Route path='/new' element={<NewTask />} />
-        <Route path='*' element={<NotFound />} />
+          <Route path='/' element={<Tasks />} />
+          <Route path='/new' element={<NewTask />} />
+          <Route path='*' element={<NotFound />} />
       </Routes>    
     </>
 
