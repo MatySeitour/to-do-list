f1afb69fa5454dd6fb6c6272a9257795d11ea749
Matias Seitour <matias.seitour01@gmail.com>
2023-02-02 16:06:49 -0300

    feat(newTask) add styles &&

--- client/src/components/NewTask.jsx
+++ client/src/components/NewTask.jsx
@@ -1,12 +1,11 @@
 import {Formik, Form} from "formik"
-import { createTaskRequest } from "../api/tasks.api";
 import { useTasks } from "../Context/TasksContext";
 
-function NewTask(){
+function NewTask({newTaskState, setNewTaskState}){
     const {handleCreate} = useTasks();
 
     return(
-        <div>
+        <div className="w-full h-full flex justify-center items-center">
             <Formik 
                 initialValues={{
                     title: "",
@@ -19,14 +18,15 @@ function NewTask(){
                 }}
             >
                 {({handleChange, handleSubmit, values, isSubmitting}) => (
-                    <Form onSubmit={handleSubmit}>
-                        <label>Title</label>
+                    <Form className="w-full h-full min-h-[400px] p-10 flex flex-col justify-center items-center bg-slate-500" onSubmit={handleSubmit}>
+                        <h2 className="text-white -translate-y-[10px] font-bold text-lg">CREATE A NEW TASK</h2>
+                        <label className="">Title</label>
                         <input name="title" type="text" placeholder="Write a title" onChange={handleChange} value={values.title}/>
                 
                         <label>Description</label>
                         <textarea name="description" rows="3" placeholder="Write a description" onChange={handleChange} value={values.description}></textarea>
                 
-                        <button type="submit" disabled={isSubmitting}>
+                        <button className="bg-white" type="submit" disabled={isSubmitting}>
                             {isSubmitting ? "Saving..." : "Save"}
                         </button>
                     </Form>
