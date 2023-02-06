e71fb0f1d7911ac4f94603fd5025b364df37b0fe
Matias Seitour <matias.seitour01@gmail.com>
2023-02-06 17:09:13 -0300

    feat(taskcontext) add task statuses and error messages to the title field

--- client/src/Context/TasksContext.jsx
+++ client/src/Context/TasksContext.jsx
@@ -1,7 +1,5 @@
 import { useState, createContext, useContext } from "react";
-import { getTasksRequest } from "../api/tasks.api";
-import { deleteTaskRequest } from "../api/tasks.api";
-import { createTaskRequest } from "../api/tasks.api";
+import { getTasksRequest, createTaskRequest, deleteTaskRequest, getTaskRequest  } from "../api/tasks.api";
 
 const TasksContext = createContext({});
 
@@ -15,7 +13,10 @@ export const useTasks = () => {
 
 export function TasksContextProvider({ children }) {
     const [tasks, setTasks] = useState([]);
-
+    const [newTaskState, setNewTaskState] = useState(false);
+    const [messageTitleError, setMessageTitleError] = useState(false);
+    const [taskEditState, setTaskEditState] = useState(false);
+    
     
     const handleDelete = async (id) => {
         try{
@@ -32,6 +33,20 @@ export function TasksContextProvider({ children }) {
         try{
             const response = await createTaskRequest(task);
             loadTasks();
+            setNewTaskState(false);
+            setMessageTitleError(false);
+        }
+        catch(error){
+            setMessageTitleError(true);
+            console.error(error)
+        }
+    }
+
+    const handleEdit = async (taskId) => {
+        try{
+            const response = await getTaskRequest(taskId);
+            setTaskEditState(true);
+            console.log(response);
         }
         catch(error){
             console.error(error)
@@ -44,7 +59,7 @@ export function TasksContextProvider({ children }) {
     }
 
     return (
-        <TasksContext.Provider value={{tasks, loadTasks, handleDelete, handleCreate}}>
+        <TasksContext.Provider value={{tasks, loadTasks, handleDelete, handleCreate, newTaskState, setNewTaskState, messageTitleError, setMessageTitleError, handleEdit, taskEditState, setTaskEditState}}>
             {children}
         </TasksContext.Provider>
     )
