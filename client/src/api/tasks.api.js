ec7ccf1eb6793fad09f895dfa12859c082349fc4
Matias Seitour <matias.seitour01@gmail.com>
2023-02-06 17:05:50 -0300

    feat(api) add the get method to fetch a task

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -5,3 +5,5 @@ export const getTasksRequest = async (tasks) => await axios.get("http://localhos
 export const createTaskRequest = async (task) => await axios.post("http://localhost:3000/tasks", task);
 
 export const deleteTaskRequest = async (taskId) => await axios.delete(`http://localhost:3000/tasks/${taskId}`);
+
+export const getTaskRequest = async (taskId) => await axios.get(`http://localhost:3000/tasks/${taskId}`);
