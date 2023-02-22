b6f8dd4a68688b7d9a35be8023d01463104ed5b9
Matias Seitour <matias.seitour01@gmail.com>
2023-02-22 15:08:53 -0300

    feat(component/nav) add nav component

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,36 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faGear, faRightFromBracket, faUser} from "@fortawesome/free-solid-svg-icons"
+import { useUsers } from "../Context/UsersContext";
+import { useState } from "react";
+import "../styles.css"
+
+function Nav(){
+
+    const {session, handleLogout} = useUsers();
+    console.log(session)
+
+    const [userSettings, setUserSettings] = useState(false);
+
+
+    return(
+        <header className="fixed top-4 z-10 h-12 w-full pl-3 pr-3">
+            <nav className="w-full h-full background-main rounded-md">
+                <ul className="w-full h-full flex justify-end items-center pr-4">
+                    <li className="ml-2 mr-2 text-lg text-[#fff] font-extrabold flex justify-center items-center">
+                        <p>{session.username}</p>
+                        <FontAwesomeIcon className="text-center ml-2" icon={faUser} />
+                    </li>
+                    <li className="ml-2 mr-2">
+                        <FontAwesomeIcon onClick={() => setUserSettings(prev => !prev)} className="text-center text-[#fff]" icon={faGear} />
+                        <div onClick={() => handleLogout()} className={userSettings ? "absolute scale-100 transition-all right-4 top-14 w-36 h-10 background-main rounded-md flex justify-around items-center" : "scale-0 transition-all"}>
+                            <FontAwesomeIcon className={userSettings ? "text-[#fff]" : "hidden"} icon={faRightFromBracket} />
+                            <p className={userSettings ? "-translate-y-[0.7px] text-[#fff] font-extrabold" : "hidden"}>Log Out</p>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export {Nav};
