b3739930ece0797f396ca75e93549a7b8a88d42e
Matias Seitour <matias.seitour01@gmail.com>
2023-02-22 15:11:09 -0300

    feat(component/taskedit) change height of the container

--- client/src/components/TaskEdit.jsx
+++ client/src/components/TaskEdit.jsx
@@ -16,12 +16,12 @@ function TaskEdit({taskIdEdit, setTaskIdEdit}){
 
     return(
         <div className="fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-70 flex justify-center items-center">
-            <div className="relative min-w-[300px] max-w-[300px] h-[500px] background-task m-4 rounded-xl p-4 flex flex-col justify-center items-center pt-3 custom-scroll shadow-2xl break-words">
+            <div className="relative min-w-[300px] max-w-[300px] h-[500px] background-task m-4 rounded-xl p-4 flex flex-col justify-center items-center pt-2 custom-scroll shadow-2xl break-words">
                 <span onClick={() => setTaskIdEdit(0)}  className="absolute left-4 top-4 cursor-pointer"><FontAwesomeIcon className="hover:scale-125 text-white transition-all text-3xl" icon={faArrowLeft} /></span>
                 <div className="w-full min-h-[400px] p-10 flex flex-col items-start">
-                    <h3 className="text-white font-bold tracking-wide pt-5 text-center w-full mb-4 text-lg">EDIT TASK</h3>
+                    <h3 className="text-white -translate-y-3 font-bold tracking-wide pt-5 text-center w-full mb-4 text-lg">EDIT TASK</h3>
                     <div className="w-full h-full flex flex-col">
-                        <label className="text-white font-semibold text-lg">title</label>
+                        <label className="text-white font-semibold text-lg tracking-wide">title</label>
                         <input className="bg-white rounded-sm outline-none p-2 text-[#a0f] focus:bg-slate-300 placeholder:text-[#a0f]" type="text"  name="title" placeholder={`write a new title`} value={`${newEditTask.title}`} onChange={(e) => {
                             setNewEditTask({
                                 title: e.target.value,
@@ -33,7 +33,7 @@ function TaskEdit({taskIdEdit, setTaskIdEdit}){
                     </div>
 
                     <div className="w-full h-full flex flex-col">
-                        <label className="text-white font-semibold text-lg">description</label>
+                        <label className="text-white font-semibold text-lg tracking-wide">description</label>
                         <textarea className="resize-none custom-scroll bg-white rounded-sm outline-none p-2 text-[#a0f] focus:bg-slate-300 placeholder:text-[#a0f]" type="text" name="description" value={`${newEditTask.description}`} placeholder={`write a new description`}  onChange={(e) => {
                             setNewEditTask({
                                 title: newEditTask.title,
@@ -43,7 +43,7 @@ function TaskEdit({taskIdEdit, setTaskIdEdit}){
                         />
                     </div>
 
-                    <div className="w-full flex justify-center translate-y-2">
+                    <div className="w-full flex justify-center translate-y-4">
                         <button className="btn w-28 bg-white outline-none border-none text-[#a0f] hover:bg-white shadow-button__editTask" onClick={() => {
                             handleEdit(taskEditData.id, newEditTask)
                             }}>save
