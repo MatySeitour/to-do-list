7a2606cb6e1432d5c17bd5c8f36d75c980c39721
Matias Seitour <matias.seitour01@gmail.com>
2023-02-22 15:06:29 -0300

    feat(client/api) change url

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,16 +1,16 @@
 import axios from "axios"
 
-export const getTasksRequest = async (tasks) => await axios.get("http://localhost:3000/tasks");
+export const getTasksRequest = async (tasks) => await axios.get("/api/tasks");
 
-export const createTaskRequest = async (task) => await axios.post("http://localhost:3000/tasks", task);
+export const createTaskRequest = async (task) => await axios.post("/api/tasks", task);
 
-export const deleteTaskRequest = async (taskId) => await axios.delete(`http://localhost:3000/tasks/${taskId}`);
+export const deleteTaskRequest = async (taskId) => await axios.delete(`/api/tasks/${taskId}`);
 
-export const getTaskRequest = async (taskId) => await axios.get(`http://localhost:3000/tasks/${taskId}`);
+export const getTaskRequest = async (taskId) => await axios.get(`/api/tasks/${taskId}`);
 
-export const editTaskRequest = async (taskId, newTask) => await axios.put(`http://localhost:3000/tasks/${taskId}`, newTask);
+export const editTaskRequest = async (taskId, newTask) => await axios.put(`/api/tasks/${taskId}`, newTask);
 
-export const toggleDoneRequest = async (taskId, doneState) => await axios.put(`http://localhost:3000/tasks/${taskId}`, {
+export const toggleDoneRequest = async (taskId, doneState) => await axios.put(`/api/tasks/${taskId}`, {
     done: doneState,
 });
 
