de11273fcb945d9d08397da4fa2015d9ad7be3ba
Matias Seitour <matias.seitour01@gmail.com>
2023-02-22 15:03:09 -0300

    feat(client/app) add userContext provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,16 @@ import App from './App'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
 import {TasksContextProvider} from './Context/TasksContext'
+import { UsersContextProvider } from './Context/UsersContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TasksContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </TasksContextProvider>
+        <BrowserRouter>
+        <UsersContextProvider>
+          <TasksContextProvider>
+            <App />
+          </TasksContextProvider>
+        </UsersContextProvider>
+        </BrowserRouter>
   </React.StrictMode>,
 )
