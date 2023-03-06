b028657c40ae630c789b00da8c6cc37d6b05e6f5
Matias Seitour <matias.seitour01@gmail.com>
2023-03-06 15:41:53 -0300

    feat(client/api) add send mail request && create new password request

--- client/src/api/login.api.js
+++ client/src/api/login.api.js
@@ -2,9 +2,13 @@ import axios from "axios";
 
 export const loginRequest = async (user) => await axios.post("/api/auth/login", user);
 
+export const sendMailToRecovery = async (inputUser) => await axios.post("/api/auth/recovery", inputUser);
+
 export const sessionRequest = async () => await axios.get("/api/auth/session");
 
 export const createUserRequest = async (newUser) => await axios.post("/api/auth/register", newUser);
+
+export const createNewPassword = async (newPassword) => await axios.post("/api/auth/new-password", newPassword)
 
 export const logoutRequest = async () => await axios.get("/api/auth/logout");
 
