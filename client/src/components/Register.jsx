ae6df17b813096e0ff5cd88d3b854fc8de5bfc79
Matias Seitour <matias.seitour01@gmail.com>
2023-03-06 15:45:15 -0300

    feat(client/component/register) add some styles

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -7,7 +7,7 @@ function Register(){
     const {handleCreateUser} = useUsers();
     return(
             <div className="background-main w-full h-full flex justify-center items-center relative">
-                <div className="w-auto min-h-[400px] bg-white h-auto p-10 pt-14 flex flex-col justify-center items-center">
+                <div className="w-auto min-h-[400px] bg-white h-auto p-10 pt-14 flex flex-col justify-center items-center rounded-md">
                 <Formik 
                     initialValues={{
                         username: "",
@@ -26,17 +26,17 @@ function Register(){
                             <div className="w-full h-full flex flex-col justify-center">
                                 <div className="w-full h-full flex flex-col mb-5">
                                     <label className="text-left font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#00f] to-[#a0f] text-lg">Username</label>
-                                    <input type="text" className="bg-white focus:placeholder:text-white text-[#a0f] focus:text-white focus:bg-gradient-to-tr from-[#00f] to-[#a0f] outline-none p-2 border border-[#a0f]" onChange={handleChange} name="username" value={values.username} />
+                                    <input type="text" className="rounded-md bg-white focus:placeholder:text-white text-[#a0f] focus:text-white focus:bg-gradient-to-tr from-[#00f] to-[#a0f] outline-none p-2 border border-[#a0f]" onChange={handleChange} name="username" value={values.username} />
                                 </div>
 
                                 <div className="w-full h-full flex flex-col mb-5">
                                     <label className="text-left font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#00f] to-[#a0f] text-lg">Password</label>
-                                    <input type="password" className="bg-white focus:placeholder:text-white text-[#a0f] focus:text-white focus:bg-gradient-to-tr from-[#00f] to-[#a0f] outline-none p-2 border border-[#a0f]" onChange={handleChange} name="password" value={values.password} />
+                                    <input type="password" className="rounded-md bg-white focus:placeholder:text-white text-[#a0f] focus:text-white focus:bg-gradient-to-tr from-[#00f] to-[#a0f] outline-none p-2 border border-[#a0f]" onChange={handleChange} name="password" value={values.password} />
                                 </div>
 
                                 <div className="w-full h-full flex flex-col mb-5">
                                     <label className="text-left font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#00f] to-[#a0f] text-lg">Email</label>
-                                    <input type="email" className="bg-white focus:placeholder:text-white text-[#a0f] focus:text-white focus:bg-gradient-to-tr from-[#00f] to-[#a0f] outline-none p-2 border border-[#a0f]" onChange={handleChange} name="email" value={values.email} />
+                                    <input type="email" className="rounded-md bg-white focus:placeholder:text-white text-[#a0f] focus:text-white focus:bg-gradient-to-tr from-[#00f] to-[#a0f] outline-none p-2 border border-[#a0f]" onChange={handleChange} name="email" value={values.email} />
                                 </div>
 
                                 <div className="w-full h-full flex justify-center mb-5">
@@ -46,7 +46,9 @@ function Register(){
                         </Form>
                     )}
                 </Formik>
-                    <Link className="text-[#a0f] underline font-semibold" to={"/login"}>Do you have an account?</Link>
+                <div className="w-full">
+                    <Link className="text-[#a0f] line-before__register relative font-semibold" to={"/login"}>Do you have an account?</Link>
+                </div>
                 </div>
             </div>
         )
