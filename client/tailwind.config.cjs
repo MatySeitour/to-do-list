09945d1f084ca40a38031a14aa350a7b824a201a
Matias Seitour <matias.seitour01@gmail.com>
2023-03-06 15:35:00 -0300

    feat(client/tailwind.config) add new animation

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -12,10 +12,19 @@ module.exports = {
           '25%': { transform: 'rotate(-3deg)' },
           '50%': { transform: 'rotate(3deg)' },
           '100%': { transform: 'rotate(0deg)' },
-        }
+        },
+        spin_gear: {
+          '0%': { transform: 'rotate(0deg)' },
+          '10%': { transform: 'rotate(180deg)' },
+        },
+        spin_gear: {
+          '0%, 100': { transform: 'rotate(0deg)' },
+          '10%': { transform: 'rotate(180deg)' },
+        },
       },
       animation: {
         wiggle: 'wiggle .5s ease',
+        spin_gear: 'spin_gear .5s ease',
       }
     },
   },
