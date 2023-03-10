ca08968265ffb2cee958441303f3bb2c19110f4e
Matias Seitour <matias.seitour01@gmail.com>
2023-03-10 10:04:12 -0300

    feat(client/newtask) add some styles to the handle error

--- client/src/components/NewTask.jsx
+++ client/src/components/NewTask.jsx
@@ -5,7 +5,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowLeft} from "@fortawesome/free-solid-svg-icons"
 
 function NewTask(){
-    const {handleCreate, newTaskState, setNewTaskState, messageTitleError} = useTasks();
+    const {handleCreate, newTaskState, setNewTaskState, setMessageTitleError, messageTitleError} = useTasks();
 
     return(
         <div className="w-full h-full flex justify-center items-center relative">
@@ -22,13 +22,17 @@ function NewTask(){
             >
                 {({handleChange, handleSubmit, values, isSubmitting}) => (
                     <Form className="w-full min-h-[400px] h-auto p-10 flex flex-col justify-center items-center" onSubmit={handleSubmit}>
-                        <FontAwesomeIcon icon={faArrowLeft} onClick={() => setNewTaskState(prevState => !prevState)} className="absolute z-20 top-4 right-4 text-3xl font-semibold text-[#a0f] hover:scale-110 cursor-pointer" />
+                        <FontAwesomeIcon icon={faArrowLeft} onClick={() => {
+                            setNewTaskState(prevState => !prevState) 
+                            setMessageTitleError(0)}
+                        } 
+                            className="absolute z-20 top-4 right-4 text-3xl font-semibold text-[#a0f] hover:scale-110 cursor-pointer" />
                         <div className="w-full h-full flex flex-col justify-center">
                         <h2 className="text-transparent bg-clip-text text-center bg-gradient-to-r from-[#00f] to-[#a0f] font-bold text-lg mb-4">CREATE NEW TASK</h2>
                             <div className="w-full h-full flex flex-col mb-5">
                                 <label className="text-left font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#00f] to-[#a0f] text-lg">Title</label>
-                                <input name="title" className="rounded-md bg-white focus:placeholder:text-white text-[#a0f] focus:text-white focus:bg-gradient-to-tr from-[#00f] to-[#a0f] outline-none p-2 border border-[#a0f]" type="text" placeholder="Write a title" onChange={handleChange} value={values.title}/>
-                                <span className="text-red-600 text-sm translate-x-1">{messageTitleError ? "This task must have title." : ""}</span>
+                                <input name="title" className={messageTitleError ? "animate-wiggle border-red-500 rounded-md bg-white focus:placeholder:text-white text-[#a0f] focus:text-white focus:bg-gradient-to-tr from-[#00f] to-[#a0f] outline-none p-2 border" : "rounded-md bg-white focus:placeholder:text-white text-[#a0f] focus:text-white focus:bg-gradient-to-tr from-[#00f] to-[#a0f] outline-none p-2 border border-[#a0f]"} type="text" placeholder="Write a title" onChange={handleChange} value={values.title}/>
+                                {messageTitleError !== 0 ? <span className="text-red-600 text-sm translate-x-1">{messageTitleError === 2 ? "This task must have title." : "the title must have less than 20 characters"}</span> : <span></span>}
                             </div>
 
                             <label className="text-left font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#00f] to-[#a0f] text-lg">Description</label>
