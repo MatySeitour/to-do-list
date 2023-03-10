7ac4d0735cd5a0c29e2232758a22ad11a411fa36
Matias Seitour <matias.seitour01@gmail.com>
2023-03-10 09:59:16 -0300

    feat(tasks) add some changes on the styles && import task details component

--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -4,14 +4,14 @@ import { Nav } from "./Nav";
 import { useTasks } from "../Context/TasksContext";
 import { NewTask } from "./NewTask";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPenToSquare, faCheck, faXmark} from "@fortawesome/free-solid-svg-icons"
+import {faPenToSquare, faCheck, faXmark, faTrashCan} from "@fortawesome/free-solid-svg-icons"
 import "../styles.css"
 import { useUsers } from "../Context/UsersContext";
+import { TaskDetail } from "./TaskDetail";
 
 function Tasks(){
-    const {tasks, loadTasks, handleDelete, newTaskState ,setNewTaskState, handleGetTask, taskIdEdit ,setTaskIdEdit, toggleDone} = useTasks();
+    const {tasks, loadTasks, handleDelete, newTaskState ,setNewTaskState, handleGetTask, taskIdEdit ,setTaskIdEdit, toggleDone, handleTask, taskDetails, setTaskDetails, taskIdDetail, setTaskIdDetail} = useTasks();
   const {session, handleLogout} = useUsers();
-  console.log("tasks", session);
 
     useEffect(() =>{
         loadTasks();
@@ -29,9 +29,9 @@ function Tasks(){
         return(
             <>
                 <Nav />
-                <main className="relative w-full min-h-full h-auto flex justify-center items-center p-4 pl-5 pt-20 bg-slate-300 overflow-hidden overflow-y-scroll custom-scroll">
+                <main className="relative w-full min-h-full h-auto flex justify-center items-start p-4 pl-5 pt-20 bg-slate-300 overflow-hidden overflow-y-scroll custom-scroll">
                 {newTaskState ? <div className="w-full h-full absolute bg-black z-10 top-0 left-0 right-0 bg-opacity-70"></div> : <div></div>}
-                <div className="w-full h-full min-h-[500px] rounded-lg bg-white">
+                <div className="w-full h-full min-h-[80vh] rounded-lg bg-white">
                     <h1 className="text-[#a0f] pl-2 pr-2 text-center mt-5 mb-5 text-2xl tracking-wider font-extrabold">WELCOME TO TASKSKER!</h1>
                     <div className="w-full flex h-auto justify-center">
                         <button onClick={() => setNewTaskState(prevState => !prevState)} className="background-main shadow-lg hover:animate-wiggle transition-all p-2 rounded-md font-extrabold text-[#fff]">CREATE A NEW TASK!</button>
@@ -55,29 +55,34 @@ function Tasks(){
                     <ul className="w-full flex justify-center items-center flex-wrap pt-4">
                         {tasks.length === 0 ? <h2 className="text-[#a0f] font-bold tracking-wider">NOT TASK CREATED YET...</h2> : <div></div>}
                         {tasks.map((task) => (
-                            <li className="relative min-w-[300px] max-w-[300px] h-[200px] background-task m-4 rounded-xl flex flex-col items-center pt-3 custom-scroll shadow-2xl break-words" key={task.id}>
-                                <h3 className="text-white text-xl tracking-wider mb-4 font-extrabold shadow-text">{task.title}</h3>
+                            <li className="relative min-w-[300px] max-w-[300px] h-[200px] background-task m-4 rounded-xl flex flex-col items-center pt-6 custom-scroll shadow-2xl break-words" key={task.id}>
+                                <h3 className="text-white text-xl tracking-wider mb-4 font-extrabold shadow-text">{task.title.length > 20 ? `${task.title.slice(0, 17)}...` : task.title}</h3>
                                 <h4 className="text-white font-extrabold inline-block shadow-text">Created on <p className="inline-block shadow-text">{task.createdAt.split("T")[0]}</p></h4>
-                                <div className="w-full flex justify-center items-center p-4">
+                                <div onClick={() => handleTask(task.id)} className="w-full flex justify-center items-center p-4">
                                     <p className="w-full text-left text-white font-bold tracking-wider">{task.description.length > 60 ? `${task.description.slice(0, 60)}...` : task.description}</p>
                                 </div>
                                 <div onClick={() => handleDelete(task.id)} className="absolute w-12 h-12 background-main cursor-pointer hover:scale-110 transition-all -right-2 -top-4 rounded-full flex justify-center items-center">
-                                    <span className="text-white text-xl font-extrabold">X</span>
+                                    <span className="text-white text-xl font-extrabold"><FontAwesomeIcon className="text-2xl" icon={faTrashCan}/></span>
                                 </div>
                                 <div onClick={() => handleGetTask(task.id)} className="absolute w-12 h-12 background-task cursor-pointer hover:scale-110 transition-all -left-2 -top-4 rounded-full flex justify-center items-center">
                                     <FontAwesomeIcon icon={faPenToSquare} className="text-white text-xl font-extrabold" />
                                 </div>
                                 {task.done === 0 ? 
-                                <div onClick={() => toggleDone(task.id, handleDone(task.done))} className="hover:scale-110 transition-all select-none w-12 h-12 bg-red-600 absolute -right-2 -bottom-2 rounded-full flex justify-center items-center text-white cursor-pointer"><FontAwesomeIcon className="text-2xl" icon={faXmark}/></div>
-                                :
-                                <div onClick={() => toggleDone(task.id, handleDone(task.done))} className="hover:scale-110 transition-all select-none w-12 h-12 bg-green-600 absolute -right-2 -bottom-2 rounded-full flex justify-center items-center text-white cursor-pointer"><FontAwesomeIcon className="text-2xl" icon={faCheck}/></div>
-                            }
+                                    <div onClick={() => toggleDone(task.id, handleDone(task.done))} className="hover:scale-110 transition-all select-none w-10 h-10 bg-red-600 absolute rounded-md -right-2 -bottom-2 flex justify-center items-center text-white cursor-pointer"><FontAwesomeIcon className="text-2xl" icon={faXmark}/></div>
+                                    :
+                                    <div onClick={() => toggleDone(task.id, handleDone(task.done))} className="hover:scale-110 transition-all select-none w-10 h-10 bg-green-600 absolute -right-2 -bottom-2 rounded-md flex justify-center items-center text-white cursor-pointer"><FontAwesomeIcon className="text-2xl" icon={faCheck}/></div>
+                                }
                             </li>
                         ))}
                         {taskIdEdit ? 
                             <TaskEdit taskIdEdit={taskIdEdit} setTaskIdEdit={setTaskIdEdit} />
                             :
                             <></>
+                        }
+                        {taskIdDetail ? 
+                            <TaskDetail taskDetails={taskDetails} setTaskIdDetail={setTaskIdDetail} toggleDone={toggleDone} handleDone={handleDone}/>
+                            :
+                            <></>    
                         }
                     </ul>
                 </div>
