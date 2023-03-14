32ac3d591697802be5a8de4994c6423c28130194
Matias Seitour <matias.seitour01@gmail.com>
2023-03-14 16:31:32 -0300

    feat(vite.config) server url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://tasker-backend-production.up.railway.app",
+        // target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
