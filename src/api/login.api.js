7ca89acef0ca2f763bf80bef6399ac33f1c43795
Matias Seitour <matias.seitour01@gmail.com>
2023-03-14 16:40:10 -0300

    [MOD] hardcoded url test

--- src/api/login.api.js
+++ src/api/login.api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-export const loginRequest = async (user) => await axios.post("/api/auth/login", user);
+export const loginRequest = async (user) => await axios.post("https://tasker-backend-production.up.railway.app/api/auth/login", user);
 
-export const sendMailToRecovery = async (inputUser) => await axios.post("/api/auth/recovery", inputUser);
+export const sendMailToRecovery = async (inputUser) => await axios.post("https://tasker-backend-production.up.railway.app/api/auth/recovery", inputUser);
 
-export const sessionRequest = async () => await axios.get("/api/auth/session");
+export const sessionRequest = async () => await axios.get("https://tasker-backend-production.up.railway.app/api/auth/session");
 
-export const createUserRequest = async (newUser) => await axios.post("/api/auth/register", newUser);
+export const createUserRequest = async (newUser) => await axios.post("https://tasker-backend-production.up.railway.app/api/auth/register", newUser);
 
-export const createNewPassword = async (newPassword) => await axios.post("/api/auth/new-password", newPassword)
+export const createNewPassword = async (newPassword) => await axios.post("https://tasker-backend-production.up.railway.app/api/auth/new-password", newPassword)
 
-export const logoutRequest = async () => await axios.get("/api/auth/logout");
+export const logoutRequest = async () => await axios.get("https://tasker-backend-production.up.railway.app/api/auth/logout");
 
--- src/api/tasks.api.js
+++ src/api/tasks.api.js
@@ -1,16 +1,16 @@
 import axios from "axios"
 
-export const getTasksRequest = async (tasks) => await axios.get("/api/tasks");
+export const getTasksRequest = async (tasks) => await axios.get("https://tasker-backend-production.up.railway.app/api/tasks");
 
-export const createTaskRequest = async (task) => await axios.post("/api/tasks", task);
+export const createTaskRequest = async (task) => await axios.post("https://tasker-backend-production.up.railway.app/api/tasks", task);
 
-export const deleteTaskRequest = async (taskId) => await axios.delete(`/api/tasks/${taskId}`);
+export const deleteTaskRequest = async (taskId) => await axios.delete(`https://tasker-backend-production.up.railway.app/api/tasks/${taskId}`);
 
-export const getTaskRequest = async (taskId) => await axios.get(`/api/tasks/${taskId}`);
+export const getTaskRequest = async (taskId) => await axios.get(`https://tasker-backend-production.up.railway.app/api/tasks/${taskId}`);
 
-export const editTaskRequest = async (taskId, newTask) => await axios.put(`/api/tasks/${taskId}`, newTask);
+export const editTaskRequest = async (taskId, newTask) => await axios.put(`https://tasker-backend-production.up.railway.app/api/tasks/${taskId}`, newTask);
 
-export const toggleDoneRequest = async (taskId, doneState) => await axios.put(`/api/tasks/${taskId}`, {
+export const toggleDoneRequest = async (taskId, doneState) => await axios.put(`https://tasker-backend-production.up.railway.app/api/tasks/${taskId}`, {
     done: doneState,
 });
 
