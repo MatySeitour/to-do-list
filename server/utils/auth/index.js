b5ef214a4ed4b3a45dd3f0814c3a40b806832f64
Matias Seitour <matias.seitour01@gmail.com>
2023-03-14 14:02:45 -0300

    BREAKING CHANGE(server) migrate the server to another repository

--- server/utils/auth/index.js
+++
@@ -1,6 +0,0 @@
-import passport from "passport";
-import { localStrategy } from "./strategies/local.strategy.js";
-import { jwtStrategy } from "./strategies/jwt.strategy.js";
-
-passport.use(localStrategy);
-passport.use(jwtStrategy);
